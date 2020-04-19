2519ea3c781418c662067f368061d434b7f55d33
Yuri Duarte <yuriduartedev@gmail.com>
2020-04-19 19:46:50 -0300

    Ajusta log do server.js

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 const app = require("./app");
 
-app.listen(process.env.PORT || 3000, () => {
-  return '🚀 Server is running...'
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+  console.log(`🚀 Server is running on port ${port}.`);
 });

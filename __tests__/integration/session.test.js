246eda92453423e1151ad6097a6a6e3e2f25b33c
Yuri Duarte <yuriduartedev@gmail.com>
2020-04-20 11:04:07 -0300

    adiciona teste de validação do jwt

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -55,7 +55,6 @@ describe('Authentication', () => {
   });
 
   it('should be able to access private routes when authenticated', async () => {
-
     const user = await factory.create('User', {
       password: '123123'
     });
@@ -65,5 +64,20 @@ describe('Authentication', () => {
       .set("Authorization", `Bearer ${user.generateToken()}`);
 
     expect(response.status).toBe(200);
+  });
+
+  it('should not be able to access private routes without jwt token', async () => {
+    const response = await request(app)
+      .get('/dashboard');
+
+    expect(response.status).toBe(401);
+  });
+
+  it('should not be able to access private routes with invalid jwt token', async () => {
+    const response = await request(app)
+      .get('/dashboard')
+      .set("Authorization", `Bearer 123123`);
+
+    expect(response.status).toBe(401);
   });
 });
